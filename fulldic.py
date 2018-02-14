38b1e277f46546c2e92111446a7c1732d03610f7
Matvey Sokolovskiy <matvey10@gmail.com>
2018-02-14 03:36:55 +0300

    problem of microframes solved. from now on the resulting xml quality depends just on the original dataframe

--- fulldic.py
+++ fulldic.py
@@ -16,11 +16,6 @@ def save_xml(filename, xml_code):
 #            lang = [1]
 #            mframe = [2]
 
-def write_txt(what, where):
-    doc = open(where, 'w', encoding='utf-8')
-    doc.write(what)
-    doc.close()
-
 name = "db_full_v2.txt"
 
 f = open (name, 'r', encoding = 'utf-8')
@@ -28,47 +23,63 @@ all_strings = f.read()
 f.close()
 strings = all_strings.split ("\n")
 
-lexdic - {}
 
-for st in strings:
+fieldic = {} #dictionary. key - field, val - LIST consisting of lists2. each list2 is a string relating to the field
+
+for string in strings:
     param = string.split("\t")
-    if param[0] not in fieldic:
-        lexdic[param[0]] = [param]
+    if param[6] not in fieldic:
+        fieldic[param[6]] = [param]
     else:
-        lexdic[param[0]].append(param)
-
-fieldic = {} #dictionary. key - field, val - list consisting of LISTS. each list is a string with the field
-for elem in lexdic:
-    dic = {}
-    for string in lexdic[elem]:
-        if string[6] not in fieldic:
-            dic[string[6]] = [param]
-        else:
-            dic[string[6]].append(param)
-    fieldic[]
+        fieldic[param[6]].append(param)
 
 fulldic = {} #key - field, val - dictionary with frames. 
 for _field_ in fieldic:
-    dic = {} #dictionary. key - frame, val - all table strings whith the frame
+    dic = {} #key – frame, val – lexdic parts
+    lexdic = {} #dictionary. key - lexeme, val - list of strings
+
     for stri in fieldic[_field_]:
-        if stri[4] not in dic:
-            dic[stri[4]] = [stri]
+        if stri[0] not in lexdic:
+            lexdic[stri[0]] = [stri]
         else:
-            dic[stri[4]].append(stri)
+            lexdic[stri[0]].append(stri)
+
+    for lex in lexdic:
+        lexframe = lexdic[lex][0][4]
+        if lexframe not in dic:
+            dic[lexframe] = {}
+            dic[lexframe][lex] = lexdic[lex]
+        else:
+             dic[lexframe][lex] = lexdic[lex]
+
     fulldic[_field_] = dic
 
-write_txt(str(fulldic), "FLDC.txt")
+    # fulldic structure:
+    # ROOTdic   key - fields, val - dic1.
+    # dic1      key - frames, val - dic2
+    # dic2      key - lexemes, val – a list of all strings with this lexeme.
+
+    # field1:
+    #   frame1:
+    #       lexeme1:
+    #           string with lexeme1 + microframe1
+    #           string with lexeme1 + microframe2
+    #           string with lexeme1 + microframe3
+    #       lexeme2:
+    #           string with lexeme2
+    #   frame2:
+    #       lexeme3:
+    # field2
 
 root = ET.Element ('root')
 root.text = ""  
 for _field_ in fulldic:
-    
-    ##field = ET.SubElement (root, 'field')
+
     field = ET.SubElement (root, 'field')
     field.text = _field_
-    dic = fulldic[_field_]
+    framedic = fulldic[_field_]
    
-    for _frame_ in dic:
+    for _frame_ in framedic:
 
         meaning = ET.Element ("meaning")
         if param [7] == "и":
@@ -82,24 +93,30 @@ for _field_ in fulldic:
         tax_class.text = param[5]
         
         frame = ET.SubElement (field, "frame", attrib = {"meaning":meaning.text, "tax_class":tax_class.text})
-        for el in dic[_frame_]:
-            frame.text = _frame_
-            lang = ET.Element ("lang")
-            lang.text = el[1]
 
+        lexedic = framedic[_frame_]
+        for lexe in lexedic:
+            frame.text = _frame_
+
+            lang = ET.Element ("lang")
+            lang.text = lexedic[lexe][0][1]
             usage = ET.Element ("usage")
-            if el[0].startswith ('-'):
+
+            if lexe.startswith ('-'):
                 usage.text = "-"
-                el[0] = el[0].strip('-')
+                lexe2 = lexe.strip('-')
             else:
                 usage.text = "+"
+                lexe2 = lexe
 
             lexeme = ET.SubElement (frame, 'lexeme', attrib = {"lang":lang.text, "usage":usage.text})
-            lexeme.text = el[0]
+            lexeme.text = lexe2
 
-            if el[2] != '':
-                mframe = ET.SubElement (lexeme, "mframe")#, attrib = {"trans":translation_micro_frame.text)
-                mframe.text = el[2]
+            for el in lexedic[lexe]:
+
+                if el[2] != '':
+                    mframe = ET.SubElement (lexeme, "mframe")#, attrib = {"trans":translation_micro_frame.text)
+                    mframe.text = el[2]
 
                # trans = ET.Element ("trans")
                # trans.text = el[5]
@@ -107,4 +124,4 @@ for _field_ in fulldic:
                # example = ET.SubElement (mframe, "example", attrib = {"trans":trans.text})
                # example.text = el[4]
 
-save_xml('xml__db_full_v2.xml', root)
+save_xml('xml_db_full_v2.xml', root)
